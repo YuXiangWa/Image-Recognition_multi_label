48183283196392d4665b3203acef127ff9292807
YuXiangWa <63444820+YuXiangWa@users.noreply.github.com>
2021-08-04 20:07:23 +0800

    Update simplevgg.py

--- model_name/simplevgg.py
+++ model_name/simplevgg.py
@@ -20,14 +20,15 @@ class SimpleVGGNet:
 		if K.image_data_format() == "channels_first":
 			inputShape = (depth, height, width)
 			chanDim = 1
-
+		# CONV => RELU => POOL
 		model.add(Conv2D(32, (3, 3), padding="same",
 			input_shape=inputShape))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(3, 3)))
 		model.add(Dropout(0.25))
-
+		
+		# (CONV => RELU) * 2 => POOL
 		model.add(Conv2D(64, (3, 3), padding="same"))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
@@ -36,7 +37,8 @@ class SimpleVGGNet:
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
 		model.add(Dropout(0.25))
-
+		
+		# (CONV => RELU) * 2 => POOL
 		model.add(Conv2D(128, (3, 3), padding="same"))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
@@ -45,13 +47,16 @@ class SimpleVGGNet:
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
 		model.add(Dropout(0.25))
-
+		
+		# 全連接層 => RELU
 		model.add(Flatten())
 		model.add(Dense(1024))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization())
 		model.add(Dropout(0.5))
-
+		
+		#使用 softmax 函數，進行單標籤分類
+		#使用 sigmoid 函數，進行多標籤分類
 		model.add(Dense(classes))
 		model.add(Activation(finalAct))
 
